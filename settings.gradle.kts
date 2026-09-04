7797802e5c571794b5c7e6da1bf2dfbd19c65c67
provocantemsk-hash <provocante.msk@gmail.com>
2026-09-04 22:00:19 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "Mitap"
 include(":app")
